98959e5ff5c53c6ec70b63031c983daf3e91a3cf
DIPAK <DIPAK@DESKTOP-E82469P>
2020-02-05 22:36:18 +0530

    First Commit

---
+++ maven_demo/src/main/java/com/epam/maven_demo/App.java
@@ -0,0 +1,16 @@
+package com.epam.maven_demo;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+       App chocolates=new App();
+       App sweets1=new App();
+       App sweets2=new App();
+       App sweets3=new App();
+       
+       
+       
+       
+    }
+}
